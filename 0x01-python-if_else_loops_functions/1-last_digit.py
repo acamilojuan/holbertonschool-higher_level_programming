ddaee45bce54d6c3840f987144989df3b024d872
acamilojuan <1600@holbertonschool.com>
2020-04-28 21:14:51 +0000

    1-last_digit.py file created

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    positive_num = number * -1
+    modul = (positive_num % 10) * -1
+else:
+    modul = number % 10
+if modul > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, modul))
+elif modul == 0:
+    print("Last digit of {} is {} and is greater than 5".format(number, modul))
+else:
+    print("Last digit of", number, "is", modul, "and is less than 6 and not 0")
