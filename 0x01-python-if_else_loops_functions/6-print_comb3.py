1d6fbde3dd4c1aa8b21079c2c02bb85be60c2e1c
acamilojuan <1600@holbertonschool.com>
2020-04-29 00:29:28 +0000

    6-print_comb3.py file modified

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 10):
-    for number2 in range((number + 1), 10):
+    for number2 in range(number + 1, 10):
         if number != number2:
             print("{}".format(number), end="")
             print("{}, ".format(number2), end="")
