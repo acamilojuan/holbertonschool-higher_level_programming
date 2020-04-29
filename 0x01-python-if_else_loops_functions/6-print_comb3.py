cc6e1b392aa34168115753ebaedbd7af61ab142c
acamilojuan <1600@holbertonschool.com>
2020-04-29 00:36:29 +0000

    6-print_comb3.py file modified

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for number in range(0, 10):
     for number2 in range(number + 1, 10):
         if number != number2:
             if number == 8 and number2 == 9:
-                 print("{}{}".format(number, number2))
+                print("{}{}".format(number, number2))
             else:
-                print("{}{}".format(number, number2), end=", ") 
+                print("{}{}".format(number, number2), end=", ")
