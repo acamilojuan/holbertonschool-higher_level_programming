7c48c708cb4e02bc15f7c0337f0fc7903cfc091b
acamilojuan <1600@holbertonschool.com>
2020-05-05 22:14:16 +0000

    1-element_at.py file fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in range(0, len(my_list)):
-        return (my_list[idx])
     if idx < 0:
         return (None)
     if idx > len(my_list):
         return (None)
+    else:
+        return (my_list[idx])
