db1db9db27c9be2769588bcbd40a44fc418dd9af
acamilojuan <1600@holbertonschool.com>
2020-05-20 02:54:07 +0000

    4-square.py file created and tested

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"Empty class Square that defines a square"
+
+
+class Square:
+    """
+    Class Square
+
+    Attributes:
+    size: Size of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Method to get a value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method to set a change in the value
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
