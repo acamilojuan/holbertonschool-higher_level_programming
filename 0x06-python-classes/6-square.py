5add5e2c15f9f0ccba07547d3101b521ed328c25
acamilojuan <1600@holbertonschool.com>
2020-05-20 03:19:07 +0000

    6-square.py instance definition fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,7 +11,7 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
-        self.position = position
+        self.__position = position
 
     @property
     def size(self):
