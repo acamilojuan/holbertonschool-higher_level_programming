d7a27fe1db94b78a426d920c1870a33728198072
acamilojuan <1600@holbertonschool.com>
2020-06-03 15:29:25 +0000

    0-lookup.py file created and tested

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Fx to return the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Dir to print all the available attributes ad methods"""
+    return dir(obj)
