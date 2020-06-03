b43c50cadab116366e340e0357c43332d86598f9
acamilojuan <1600@holbertonschool.com>
2020-06-03 16:52:05 +0000

    3-is_kind_of_class.py file created and tested

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Fx to return True/False if obj is or not instance of a class inherited"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Methot isinstance() to check True or False"""
+    return(isinstance(obj, a_class))
