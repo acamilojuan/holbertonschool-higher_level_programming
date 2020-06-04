5af84b832fce752a812ffe0296aab90ded285db8
acamilojuan <1600@holbertonschool.com>
2020-06-04 02:50:22 +0000

    9-add_item.py file fixed

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,10 +10,10 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 if __name__ == "__main__":
     """Method to add the arguments and print"""
     filename = "add_item.json"
-    
+
     if os.path.isfile(filename):
-        my_obj = load_from_json_file(filename)
-        my_obj += sys.argv[1:]
-        save_to_json_file(list(my_obj), filename)
+        list1 = load_from_json_file(filename)
+        list1 += sys.argv[1:]
+        save_to_json_file(list(list1), filename)
     else:
         save_to_json_file(list(sys.argv[1:]), filename)
