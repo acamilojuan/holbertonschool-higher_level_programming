01950606a6e11c5d51060eaa48f01b662c8dccc7
acamilojuan <1600@holbertonschool.com>
2020-06-04 13:40:24 +0000

    2-matrix_divided.py file fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,6 +7,10 @@ def matrix_divided(matrix, div):
     matrix: list of integers to be used
     div: Number that divides
     """
+    err1 = "matrix must be a matrix (list of lists) of integers/floats"
+    err2 = "Each row of the matrix must have the same size"
+    err3 = "div must be a number"
+    err4 = "division by zero"
     leng = len(matrix[0])
     if leng is 0:
         raise TypeError(error_type_matrix)
@@ -17,9 +21,12 @@ def matrix_divided(matrix, div):
         for element in row:
             if type(element) is not int and type(element) is not float:
                 raise TypeError(error_type_matrix)
-        if type(div) is not int and type(div) is not float:
-            raise TypeError(error_type_div)
-        if div is 0:
-            raise ZeroDivisionError(error_div_zero)
-        return [[round(float(element)/float(div), 2) for element in row]
+        
+    if type(div) is not int and type(div) is not float:
+        raise TypeError(error_type_div)
+    if div is 0:
+        raise ZeroDivisionError(error_div_zero)
+    new_matrix = [[round(float(element)/float(div), 2) for element in row]
                 for row in matrix]
+    
+    return new_matrix
