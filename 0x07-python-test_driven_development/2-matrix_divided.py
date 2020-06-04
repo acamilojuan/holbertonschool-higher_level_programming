22b4aeddb09a85acfb8d0c23878130208941ac14
acamilojuan <1600@holbertonschool.com>
2020-06-04 13:50:10 +0000

    2-matrix_divided.py file fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"Function to print divided numbers"
+"""Fx to divide all objects of a matrix"""
 
 
 def matrix_divided(matrix, div):
-    """Arguments
-    matrix: list of integers to be used
-    div: Number that divides
+    """Method to divide the objents
+    Arguments: matrix, div.
+    Raises: Errors depends on the case
+    Returns: objents divided
     """
     err1 = "matrix must be a matrix (list of lists) of integers/floats"
     err2 = "Each row of the matrix must have the same size"
     err3 = "div must be a number"
     err4 = "division by zero"
-    leng = len(matrix[0])
-    if leng is 0:
-        raise TypeError(error_type_matrix)
+    if len(matrix[0]) is 0:
+        raise TypeError(err1)
 
     for row in matrix:
-        if leng != len(row):
-            raise TypeError(error_length)
-        for element in row:
-            if type(element) is not int and type(element) is not float:
-                raise TypeError(error_type_matrix)
-        
-    if type(div) is not int and type(div) is not float:
-        raise TypeError(error_type_div)
+        if len(matrix[0]) != len(row):
+            raise TypeError(err2)
+        for obj in row:
+            if type(obj) not in (int, float):
+                raise TypeError(err1)
+
+    if type(div) not in (int, float):
+        raise TypeError(err3)
+
     if div is 0:
-        raise ZeroDivisionError(error_div_zero)
-    new_matrix = [[round(float(element)/float(div), 2) for element in row]
-                for row in matrix]
-    
-    return new_matrix
+        raise ZeroDivisionError(err4)
+
+    return [[round((obj / div), 2) for obj in row] for row in matrix]
