b12ba1b9b16d018669bf68e2cd3dd769d33393e5
acamilojuan <1600@holbertonschool.com>
2020-06-09 17:49:14 +0000

    rectangle.py pep8 fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,17 +9,17 @@ class Rectangle(Base):
     """Class Rectangle that inherits from Class Base
     Attributes: self, width, height, x, y and id.
     """
-    def __init__(self, width, height, x=0, y=0, id=None): 
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         self.__width = width
@@ -27,7 +27,7 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         self.__height = height
@@ -35,7 +35,7 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         self.__x = x
@@ -43,7 +43,7 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         self.__y = y
