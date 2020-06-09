532cff2b7525fe40aa60e499a5b9a77b7f61cf39
acamilojuan <1600@holbertonschool.com>
2020-06-09 22:31:42 +0000

    test_base.py file added ot full

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Importing modules and classes needed to test
+Definition of cases
+"""
+import unittest
+import pep8
+from models.base import Base
+
+
+class TestsBase(unittest.TestCase):
+    """Class to test the Base cases"""
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/base.py'])
+        self.assertEqual(result.total_errors, 0)
