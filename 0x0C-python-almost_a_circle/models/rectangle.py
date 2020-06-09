067f45eada411c03cc1713e78f474c6bd1c2e9ba
acamilojuan <1600@holbertonschool.com>
2020-06-09 21:35:25 +0000

    rectangle.py setter fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,10 @@ class Rectangle(Base):
     Attributes: self, width, height, x, y and id.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -22,7 +22,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -30,7 +30,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
@@ -38,7 +38,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
@@ -46,4 +46,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.__y = y
+        self.__y = value
