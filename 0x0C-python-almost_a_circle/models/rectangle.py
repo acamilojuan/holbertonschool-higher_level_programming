63652c516c4591348c214c81889a9923e518f1f7
acamilojuan <1600@holbertonschool.com>
2020-06-09 22:21:40 +0000

    rectangle.py task 6 added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,7 @@ class Rectangle(Base):
         """Method to display char # in stdout"""
         for a in range(self.__height):
                 print('#' * self.__width)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height))
