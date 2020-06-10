165d8e6ee0ee350460a4df7491abd5d4f62c042a
acamilojuan <1600@holbertonschool.com>
2020-06-10 02:10:47 +0000

    base.py fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Method to use Json"""
-        if list_dictionaries is None or not list_dictionaries:
-            return "[]"
+        if list_dictionaries is None or not bool(list_dictionaries):
+            return("[]")
         else:
             return json.dumps(list_dictionaries)
