962f0ad590d4af96e951af99e79d527bce33135f
acamilojuan <1600@holbertonschool.com>
2020-06-10 01:02:32 +0000

    square.py file updated to test task 0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,49 +1,13 @@
 #!/usr/bin/python3
-"""Class Rectangle creation"""
+"""Class Square creation"""
 
 
 from models.base import Base
+from models.rectangle import Rectangle
 
 
-class Rectangle(Base):
-    """Class Rectangle that inherits from Class Base
-    Attributes: self, width, height, x, y and id.
+class Square(Rectangle):
+    """Class Square creation that inherits from Class Rectangle
     """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id)
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.__width = width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        self.__height = height
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        self.__x = x
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        self.__y = y
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
