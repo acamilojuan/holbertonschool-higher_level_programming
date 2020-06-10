1689e25989a303c653209a7f3c2104f10529603d
acamilojuan <1600@holbertonschool.com>
2020-06-10 17:17:40 +0000

    base.py task 19 added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,3 +57,20 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        name = cls.__name__ + '.json'
+        emptylist = []
+        list3 = []
+        try:
+            with open(name, mode='r', encoding='UTF8') as xfile:
+                aux = xfile.read()
+                emptylist = cls.from_json_string(aux)
+                """List of dicts"""
+                for elem in emptylist:
+                    aux2 = cls.create(**elem)
+                    list3.append(aux2)
+                return list3
+        except FileNotFoundError:
+            return emptylist
