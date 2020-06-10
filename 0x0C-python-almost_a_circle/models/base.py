67def6b95eb4ccd334b0cb1ba7f16e8b38fb5129
acamilojuan <1600@holbertonschool.com>
2020-06-10 02:12:30 +0000

    base.py fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Method to use Json"""
-        if list_dictionaries is None or not bool(list_dictionaries):
+        if list_dictionaries is None or not list_dictionaries:
             return("[]")
         else:
             return json.dumps(list_dictionaries)
