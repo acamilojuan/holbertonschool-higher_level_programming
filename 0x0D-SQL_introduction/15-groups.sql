a500ccec8ecc98ae1ce3af58a5da86d75ffa4f62
acamilojuan <1600@holbertonschool.com>
2020-07-13 21:44:00 -0500

    15-groups.sql file created

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
--- script that lists the number of records with the same score in
+-- script that lists all records of
 -- the table second_table of the database hbtn_0c_0 in your MySQL server;
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
