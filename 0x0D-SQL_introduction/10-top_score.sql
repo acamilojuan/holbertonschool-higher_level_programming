6d8016634f8d85631beb650cbb1d8dfb9bd6a9fc
acamilojuan <1600@holbertonschool.com>
2020-07-13 20:06:23 -0500

    10-top_score.sql file created and tested

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table second_table of
+-- the database hbtn_0c_0 in your MySQL server;
+SELECT second_table.score AS 'score', name FROM second_table ORDER BY score DESC;
