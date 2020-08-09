86036a5bd185f13b3117971a43f19e74366cbf9c
acamilojuan <1600@holbertonschool.com>
2020-08-09 17:11:03 -0500

    Task 3 created and tested

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """Initialize connection"""
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states\
+        WHERE BINARY name = %s;", (argv[4], ))
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
