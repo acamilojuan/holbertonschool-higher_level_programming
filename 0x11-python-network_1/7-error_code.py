cc9c95ee332f4568740bd327b6b6e8e0c5bbbc87
acamilojuan <1600@holbertonschool.com>
2020-08-24 20:09:10 -0500

    Task 7 modified and tested

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,9 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        query = requests.get(sys.argv[1])
-        print(query.text)
-    except HTTPError as error:
-        print('Error code:', query.status_code)
+    response = requests.get(sys.argv[1])
+    st_code = response.status_code
+    if st_code < 400:
+        print(st_code.text)
+    else:
+        print('Error code:', st_code)
