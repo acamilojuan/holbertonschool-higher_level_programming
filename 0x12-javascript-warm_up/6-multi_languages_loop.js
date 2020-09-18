d4b0422f1a2e41c97a8c6381e62f4eb9da24d996
acamilojuan <1600@holbertonschool.com>
2020-09-18 15:20:31 -0500

    Task 6 created and tested

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Script that prints 3 lines: using an array of string and a loop */
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index]);
+}
