c66ab7e5d5ae1a8d9d011bcc8589cc15b1778302
acamilojuan <1600@holbertonschool.com>
2020-09-18 14:53:00 -0500

    Task 5 created and tested

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Script that prints My number: <first argument converted in integer> */
+
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
+}
