d566670f909544fd909d2182331b8120d7bbd9e6
acamilojuan <1600@holbertonschool.com>
2020-09-21 14:38:26 -0500

    Task 1 created and tested

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that creates an empty class Rectangle that defines a rectangle //
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
