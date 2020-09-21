b064ee30d2e635b122eea3e3f0aeb98c1acc8056
acamilojuan <1600@holbertonschool.com>
2020-09-21 13:30:08 -0500

    Task 13 variable names fixed

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Function that returns the addition of 2 integers. //
 
-exports.add = function (num_1, num_2) {
-  return (num_1 + num_2);
+exports.add = function (a, b) {
+  return (a + b);
 };
