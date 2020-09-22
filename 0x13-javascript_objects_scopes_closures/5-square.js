5703b57b13c261e038724466736dfe0ba8bc5e9a
acamilojuan <1600@holbertonschool.com>
2020-09-21 19:16:29 -0500

    Task 5 created and tested

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/* Script that creates a class Write a class Square
+that defines a square and inherits from Rectangle of 4-rectangle.js */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index1 = 0; index1 < this.height; index1++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
