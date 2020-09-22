bf4db47ae5e0f2196929e4f683e127471a0ea581
acamilojuan <1600@holbertonschool.com>
2020-09-21 21:41:52 -0500

    Task 8 created and tested

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-/* Script that creates a function that returns the number of occurrences in a list */
+/* Script that creates a function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
   const reversed = [];
-  for (let index = list.length; index >= 0; index--) {
+  for (let index = list.length - 1; index >= 0; index--) {
     reversed.push(list[index]);
   }
   return reversed;
